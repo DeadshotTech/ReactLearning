78aaab1625a6c58156e13f8668992110b11df328
Deadshot_Tech <vishalgautam.tech@gmail.com>
2023-09-24 23:20:46 +0530

    (feature)Added more generic logic to the StartRating component. Proptypes validations added.

--- 07-usepopcorn/src/index.js
+++ 07-usepopcorn/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { useState } from "react";
 // import "./index.css";
 // import App from "./App";
 import StarRating from "./StarRating";
@@ -8,8 +9,20 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App/> */}
-    <StarRating maxRating={7} />
-    <StarRating maxRating={10} />
-    <StarRating />
+    <StarRating messages={["E", "D", "C", "B", "A"]} />
+    <StarRating messages={["E", "D", "C", "B", "A"]} defaultRating={2} />
+    <StarRating maxRating={10} size={24} />
+    <StarRating maxRating={7} color="red" className="test" />
+    <Test />
   </React.StrictMode>
 );
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
